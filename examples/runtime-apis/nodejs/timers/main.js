7fed66a8fc51e8d7f1f88c998992fd0ec899a918
José Filho <jc.bsfilho@gmail.com>
2024-10-25 17:24:03 -0300

    feat: add examples for Node.js runtime APIs
    - string-decoder, timers, url

---
+++ examples/runtime-apis/nodejs/timers/main.js
@@ -0,0 +1,25 @@
+/**
+ * An Example of using Node.js Timers API in a Azion Edge Function.
+ * Support:
+ * - Partially supported (Extended by library `timers-browserify`)
+ * @module runtime-apis/nodejs/timers/main
+ * @example
+ * // Execute with Azion Bundler:
+ * npx edge-functions build
+ *
+ */
+import timers from "node:timers";
+
+/**
+ * An example of using the Node.js Timers API in a Azion Edge Function.
+ * @param {*} event
+ * @returns {Promise<Response>}
+ */
+const main = async (event) => {
+  console.log("Hello, world!");
+  console.log("Waiting for 5 seconds...");
+  await new Promise((resolve) => timers.setTimeout(resolve, 2000));
+  return new Response("Done!", { status: 200 });
+};
+
+export default main;
---
+++ examples/runtime-apis/nodejs/url/main.js
@@ -0,0 +1,42 @@
+/**
+ * An Example of using Node.js URL API in a Azion Edge Function.
+ * Support:
+ * - Partially supported (Extended by library `url`)
+ * @module runtime-apis/nodejs/url/main
+ * @example
+ * // Execute with Azion Bundler:
+ * npx edge-functions build
+ * npx edge-functions dev
+ */
+import url from "node:url";
+
+/**
+ * An example of using the Node.js URL API in a Azion Edge Function.
+ * @param {*} event
+ * @returns {Promise<Response>}
+ */
+const main = async (event) => {
+  /**
+   * URL globalThis object
+   * https://developer.mozilla.org/en-US/docs/Web/API/URL
+   * if use URL in the browser, don't need to import
+   */
+  const newUrl = new URL("https://example.com/some/path?format=json&page=1");
+  console.log("newURL", newUrl);
+
+  const urlFormated = url.format({
+    protocol: "https",
+    hostname: "example.com",
+    pathname: "/some/path",
+    query: {
+      page: 1,
+      format: "json",
+    },
+  });
+
+  console.log(url.parse(urlFormated));
+
+  return new Response("Done!", { status: 200 });
+};
+
+export default main;
