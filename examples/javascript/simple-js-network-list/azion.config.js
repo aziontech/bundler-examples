1b984d6b754f157bf7f128dd91d285301a1f41c0
José Filho <jc.bsfilho@gmail.com>
2025-07-11 14:29:06 -0300

    fix: fixing azion.config for e2e tests (#76)

--- examples/javascript/simple-js-network-list/azion.config.js
+++ examples/javascript/simple-js-network-list/azion.config.js
@@ -1,6 +1,24 @@
+/**
+ * This file was automatically generated based on your preset configuration.
+ *
+ * For better type checking and IntelliSense:
+ * 1. Install azion as dev dependency:
+ *    npm install -D azion
+ *
+ * 2. Use defineConfig:
+ *    import { defineConfig } from 'azion'
+ *
+ * For more configuration options, visit:
+ * https://github.com/aziontech/azion
+ */
+
 module.exports = {
   build: {
     preset: "javascript",
+    entry: ["index.js"],
+    bundler: "esbuild",
+    polyfills: false,
+    worker: true,
   },
   networkList: [
     {
@@ -9,4 +27,21 @@ module.exports = {
       listContent: ["127.0.0.0/8", "10.0.0.1"],
     },
   ],
+  functions: [
+    {
+      name: "my-javascript-function",
+      path: ".edge/worker",
+    },
+  ],
+  rules: {
+    request: [
+      {
+        name: "Execute Edge Function",
+        match: "^\\/",
+        behavior: {
+          runFunction: "my-javascript-function",
+        },
+      },
+    ],
+  },
 };
