e121d5c011279dfe7f16db53fcac78379793ff7d
José Filho <jc.bsfilho@gmail.com>
2025-08-18 10:40:50 -0300

    refactor: update azion.config.cjs with new configuration structure

--- examples/runtime-apis/nodejs/fs/azion.config.cjs
+++ examples/runtime-apis/nodejs/fs/azion.config.cjs
@@ -8,36 +8,97 @@
  * 2. Use defineConfig:
  *    import { defineConfig } from 'azion'
  *
+ * 3. Replace the configuration with defineConfig:
+ *    export default defineConfig({
+ *      // Your configuration here
+ *    })
+ *
  * For more configuration options, visit:
- * https://github.com/aziontech/azion
+ * https://github.com/aziontech/lib/tree/main/packages/config
  */
 
 module.exports = {
   build: {
     preset: "javascript",
-    entry: ["index.js"],
     polyfills: true,
-    worker: false,
     memoryFS: {
       injectionDirs: ["files/"],
       removePathPrefix: "files/",
     },
   },
-  functions: [
+  edgeFunctions: [
     {
-      name: "my-javascript-function",
-      path: ".edge/worker.js",
+      name: "$EDGE_FUNCTION_NAME",
+      path: "./functions/index.js",
     },
   ],
-  rules: {
-    request: [
-      {
-        name: "Execute Edge Function",
-        match: "^\\/",
-        behavior: {
-          runFunction: "my-javascript-function",
+  edgeApplications: [
+    {
+      name: "$EDGE_APPLICATION_NAME",
+      rules: {
+        request: [
+          {
+            name: "Execute Edge Function",
+            description: "Execute edge function for all requests",
+            active: true,
+            criteria: [
+              [
+                {
+                  variable: "${uri}",
+                  conditional: "if",
+                  operator: "matches",
+                  argument: "^/",
+                },
+              ],
+            ],
+            behaviors: [
+              {
+                type: "run_function",
+                attributes: {
+                  value: "$EDGE_FUNCTION_NAME",
+                },
+              },
+            ],
+          },
+        ],
+      },
+      functionsInstances: [
+        {
+          name: "$EDGE_FUNCTION_INSTANCE_NAME",
+          ref: "$EDGE_FUNCTION_NAME",
+          args: {
+            environment: "production",
+          },
+        },
+      ],
+    },
+  ],
+  workloads: [
+    {
+      name: "$WORKLOAD_NAME",
+      active: true,
+      infrastructure: 1,
+      protocols: {
+        http: {
+          versions: ["http1", "http2"],
+          httpPorts: [80],
+          httpsPorts: [443],
+          quicPorts: null,
         },
       },
-    ],
-  },
+      deployments: [
+        {
+          name: "$DEPLOYMENT_NAME",
+          current: true,
+          active: true,
+          strategy: {
+            type: "default",
+            attributes: {
+              edgeApplication: "$EDGE_APPLICATION_NAME",
+            },
+          },
+        },
+      ],
+    },
+  ],
 };
